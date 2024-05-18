8f49946586aa0a79107cfd6d043988450a10e980
Matjasunio <153275335+Matjasunio@users.noreply.github.com>
2024-05-18 13:21:48 +0200

    Update Ramka.java

--- Ramka.java
+++ Ramka.java
@@ -8,32 +8,26 @@ import java.io.File;
 public class Ramka extends JFrame implements ActionListener {
     JButton b1, b2, b3, b4, b5, b6, b7, b8, b9;
     JLabel t1;
-    JLabel background;
     Labirynt labirynt = new Labirynt();
 
     public Ramka() {
         setSize(800, 800);
         setTitle("CRAZY MAZE");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
 
-        background = new JLabel(new ImageIcon("C:\\Users\\czarn\\Desktop\\programowanie\\tlojava.jpg"));
+        JLabel background = new JLabel(new ImageIcon("C:\\Users\\czarn\\Desktop\\programowanie\\tlojava.jpg"));
         background.setBounds(0, 0, 800, 800);
+        add(background);
         background.setLayout(null);
 
-        add(background);
-        b1 = new JButton("Lab. tekstowy");
-        b1.setIcon(null);
-        b1.setToolTipText("Wczytanie lab. tekstowego");
+        b1 = new JButton("Wczyt. labiryntu");
+        b1.setToolTipText("Wczytanie labiryntu");
         b1.setBackground(Color.GREEN);
         b1.setBounds(20, 20, 125, 40);
         background.add(b1);
         b1.addActionListener(this);
 
-        b2 = new JButton("Lab. binarny");
-        b2.setToolTipText("Wczytanie lab. binarnego");
-        b2.setBackground(Color.GREEN);
-        b2.setBounds(20, 80, 125, 40);
-        background.add(b2);
-        b2.addActionListener(this);
 
         b3 = new JButton("EXIT");
         b3.setToolTipText("Konczy dzialanie programu");
@@ -48,8 +42,8 @@ public class Ramka extends JFrame implements ActionListener {
         t1.setFont(new Font("SansSerif", Font.BOLD, 14));
         background.add(t1);
 
-        b4 = new JButton("Wyswietl lab.");
-        b4.setToolTipText("Wyswietla wczytany labirynt");
+        b4 = new JButton("Stworz JPG");
+        b4.setToolTipText("Tworzy labirynt w postaci graficznej");
         b4.setBackground(Color.GREEN);
         b4.setBounds(150, 20, 125, 40);
         background.add(b4);
@@ -86,9 +80,17 @@ public class Ramka extends JFrame implements ActionListener {
         b9 = new JButton("CLEAR");
         b9.setToolTipText("Czysci obecny komunikat");
         b9.setBackground(Color.GREEN);
-        b9.setBounds(410, 80, 125, 40);
+        b9.setBounds(20, 80, 125, 40);
         background.add(b9);
         b9.addActionListener(this);
+
+        
+        ColorPanel colorPanel = new ColorPanel();
+        JScrollPane scrollPane = new JScrollPane(colorPanel);
+        scrollPane.setBounds((getWidth() - 700) / 2, getHeight() - 600, 700, 600); 
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        background.add(scrollPane);
 
         setVisible(true);
     }
@@ -96,7 +98,7 @@ public class Ramka extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         Object zrodlo = e.getSource();
         if (zrodlo == b1) {
-            t1.setText("Wczytuje lab. tekstowy...");
+            t1.setText("Wczytuje labirynt...");
             JFileChooser fileChooser = new JFileChooser();
             int response = fileChooser.showOpenDialog(null);
             if (response == JFileChooser.APPROVE_OPTION) {
@@ -108,18 +110,11 @@ public class Ramka extends JFrame implements ActionListener {
                 labirynt.wyswietlLabirynt();
             }
 
-        } else if (zrodlo == b2) {
-            t1.setText("Wczytuje lab. binarny...");
-            JFileChooser fileChooser = new JFileChooser();
-            int response = fileChooser.showOpenDialog(null);
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                System.out.println(file);// pod file kryje sie sciezka do pliku ktora podamy do metody
-            }
+         
         } else if (zrodlo == b3) {
             dispose();
         } else if (zrodlo == b4) {
-            t1.setText("Wyswietlam labirynt...");
+            t1.setText("Tworze obrazek...");
 
         } else if (zrodlo == b5) {
             t1.setText("Wyszukuje najkrotsza sciezke...");
@@ -134,5 +129,4 @@ public class Ramka extends JFrame implements ActionListener {
         }
 
     }
-
 }
